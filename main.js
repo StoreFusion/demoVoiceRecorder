6dddd72f869cbf396d8a6b76b727078381db06e3
StoreFusion <tdur@protonmail.com>
2023-10-20 19:35:07 +0530

    [initial changes]

---
+++ main.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector("canvas");
+
+// Optional frames per second argument.
+const stream = canvas.captureStream(25);
+const recordedChunks = [];
+
+console.log(stream);
+const options = { mimeType: "video/webm; codecs=vp9" };
+const mediaRecorder = new MediaRecorder(stream, options);
+
+mediaRecorder.ondataavailable = handleDataAvailable;
+mediaRecorder.start();
+
+function handleDataAvailable(event) {
+  console.log("data-available");
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
+    console.log(recordedChunks);
+    download();
+  } else {
+    // …
+  }
+}
+function download() {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+// demo: to download after 9sec
+setTimeout((event) => {
+  console.log("stopping");
+  mediaRecorder.stop();
+}, 9000);
